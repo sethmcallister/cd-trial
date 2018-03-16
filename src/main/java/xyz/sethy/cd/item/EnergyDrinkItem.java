acdaf5991fcfe49a72aa8a0a81fd8bb09b454cc8
Seth McAllister <dozingducklet@gmail.com>
2018-03-16 16:42:53 +0000

    Documented EnergyDrinkItem

--- src/main/java/xyz/sethy/cd/item/EnergyDrinkItem.java
+++ src/main/java/xyz/sethy/cd/item/EnergyDrinkItem.java
@@ -7,12 +7,14 @@ import net.minecraft.item.Item;
 
 public class EnergyDrinkItem extends Item {
 	public EnergyDrinkItem() {
+		// Calls the constructor method in the super class, with the argument of '8080', which is the itemID
 		super(8080);
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister iconRegister) {
+		// Sets the itemIcon to the icon found in the iconRegistry by the mod 'cd', and the name 'EnergyDrink'
 		itemIcon = iconRegister.registerIcon("cd:EnergyDrink");
 	}
 }
